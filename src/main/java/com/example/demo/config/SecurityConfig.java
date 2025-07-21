da700e20e621ad9cf81a63e1d9c5326391bf0016
ManvendraS21 <manvendra.singh@pickrr.com>
2025-07-21 15:04:07 +0530

    added login page

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(registry->{
-                    registry.requestMatchers("/register/**","/quotes","/loginWithGoogle").permitAll();
+                    registry.requestMatchers("/register/**","/quotes","/loginWithGoogle","/login").permitAll();
                     registry.requestMatchers("/admin/**").hasRole("ADMIN");
                     registry.requestMatchers("/user/**").hasRole("USER");
                     registry.anyRequest().authenticated();
