e9f079f0fd3bce997405fa9cf2724b865aa97a04
bedantsharma-sudo <bedant.sharma@pickrr.com>
2025-07-21 16:36:11 +0530

    added the google login feature

--- src/main/java/com/example/demo/helper/JwtUtil.java
+++ src/main/java/com/example/demo/helper/JwtUtil.java
@@ -1,11 +1,17 @@
 package com.example.demo.helper;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -14,25 +20,34 @@ import java.util.Date;
 @Slf4j
 public class JwtUtil {
     @Value("${jwt.secret}")
-    private String secretKey;
+    private String secret;
 
-    public String generateToken(String email){
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        // Convert string to SecretKey
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.HOURS)))
-                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .signWith(secretKey)
                 .compact();
     }
 
-    public boolean validateToken(String token){
-        try{
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+    public boolean validateToken(String token) {
+        try {
+            JwtParser parser = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build();
+
+            Claims claims = parser.parseClaimsJws(token).getBody();
             return true;
-        }
-        catch (Exception e){
-            System.out.println("invalid token");
-            log.error("invalid token because: ", e);
+        } catch (Exception e) {
+            System.out.println("Invalid token");
+            log.error("Invalid token because: ", e);
             return false;
         }
     }
